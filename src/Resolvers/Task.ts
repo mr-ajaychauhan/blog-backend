f8e5e849c52a9ad8fcf1a7ab05fc6e77bfef2aec
Ajay Chauhan <ajay9670chauhan@gmail.com>
2023-04-27 08:22:11 +0530

    added mutation code

--- src/Resolvers/Task.ts
+++ src/Resolvers/Task.ts
@@ -1,11 +1,20 @@
-import { Query, Resolver } from "type-graphql";
+import { Task } from "src/entities/Task";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class TaskResolver {
     @Query(() => [String])
     async Task() {
-        let task:string[];
+        let task: string[];
         task = ["homecleaning", "bartandhona", "Khanabanana", "kapdedhona"];
         return task;
+    }
+
+    @Mutation(() => Task)
+    createTask(
+        @Arg("title", () => String)
+        title: string
+    ):Promise<Task> {
+        return Task.create({ title, isComplete: false }).save();
     }
 }
